f7245c9bb4c33b9c6347ef0b18673dd94eafd3f4
mbanx <adi.lambang@yahoo.co.id>
2020-04-28 10:56:51 +0700

    message

---
+++ src/test/java/com/tritronik/gcp/laundry/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tritronik.gcp.laundry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
