71f7b0447f09c57f21460078e7228db2d2be64d0
mbanx <adi.lambang@yahoo.co.id>
2020-04-30 16:45:37 +0700

    ddd

--- src/main/java/com/tritronik/gcp/laundry/service/controller/BasicController.java
+++ src/main/java/com/tritronik/gcp/laundry/service/controller/BasicController.java
@@ -4,8 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tritronik.gcp.laundry.service.props.ServiceProperties;
 
+@RefreshScope
 @RestController
 @RequestMapping("/basic")
 public class BasicController {
